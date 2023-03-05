d2711f49a8caf010422d7a5aac7113c7d27ee91d
MarkoSopta <philoyd99@gmail.com>
2023-03-05 19:03:36 +0100

    project init

---
+++ product-service/src/test/java/com/marko/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marko.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
